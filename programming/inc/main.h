e4b76f317b7cd95daa24a3d4cf1cf9c45d19b695
Szymon Bartosik <szymon.bartosik75@gmail.com>
2025-07-02 01:25:41 +0200

    Zmiana zlacza PDI, dodany makefile i struktura plikow

---
+++ programming/inc/main.h
@@ -0,0 +1,23 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+// Hardware configuration
+#include "config.h"
+
+// Standard C libraries
+#include <stdlib.h>
+#include <stdbool.h>
+
+// Standard AVR libraries
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+
+// External libraries
+
+
+// External variables
+
+
+#endif
